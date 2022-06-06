50add3eab5f63c76e50c9a421fc9f3d2fe34cd24
siyussla <92722334+siyussla@users.noreply.github.com>
2022-06-06 20:23:36 +0800

    Add files via upload

---
+++ solutions/Queue.java
@@ -0,0 +1,62 @@
+
+package Payment;
+
+import java.util.LinkedList;
+
+import java.util.LinkedList;
+
+public class Queue<E> {
+    
+    private java.util.LinkedList<E> list = new java.util.LinkedList<>();
+    
+    public Queue(E[] e){
+        list = new LinkedList<>();
+        for (int i = 0; i < e.length; i++) {
+            list.addLast(e[i]);
+        }
+    }
+
+    public Queue() {
+        list = new LinkedList<>();
+    }
+    
+    public void enqueue(E e){
+        list.addLast(e);
+    }
+    
+    public E dequeue(){
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.removeFirst();
+    }
+    
+    public E getElement(int i){
+        if (i<0 || i>list.size()) {
+            return null;
+        }
+        return list.get(i);
+    }
+    
+    public E peek(){
+        return list.getFirst();
+    }
+    
+    public int getSize(){
+        return list.size();
+    }
+    
+    public boolean contains(E e){
+        return list.contains(e);
+    }
+    
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
+    
+}
