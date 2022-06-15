3a3abc4915cd4343f66c750a68af960d183c7e00
siyussla <92722334+siyussla@users.noreply.github.com>
2022-06-15 21:30:48 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,18 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author user
- */
 import java.util.*;
 
-/**
- *
- * @author nisyaqanita
- */
 public class Payment implements Comparable<Payment>{
     
     // variable block
