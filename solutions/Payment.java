2e44b0315beef8853beee6b421820ea6cecc8b0f
siyussla <92722334+siyussla@users.noreply.github.com>
2022-06-16 17:33:38 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,141 +1,146 @@
 
-
 import java.util.*;
 
-public class Payment implements Comparable<Payment> {
-
+public class Payment implements Comparable<Payment>{
+    
+    // variable block
     private Long oldEpoch;
     private Long newEpoch;
     private Long newEpochHolder;
-    private String txnld, tier;
+    private String txnId;
+    private String tier;
+    
     final String PLATINUM = "PLATINUM";
     final String GOLD = "GOLD";
     final String SILVER = "SILVER";
-
-    static PriorityQueue<Payment> meows = new PriorityQueue<>();
+    
+    static PriorityQueue<Payment> meowsPQ = new PriorityQueue<>();
     private static Stack<Long> stack = new Stack<>();
-
-    public Payment(Long oldEpoch, String txnld, String tier) {
+    // end of variale block
+    
+    public Payment(Long oldEpoch, String txnId, String tier) {
         if (tier.equalsIgnoreCase(PLATINUM)) {
             this.newEpoch = oldEpoch - 3000;
-        } else if (tier.equalsIgnoreCase(GOLD)) {
+        }
+        else if (tier.equalsIgnoreCase(GOLD)) {
             this.newEpoch = oldEpoch - 2000;
-        } else if (tier.equalsIgnoreCase(SILVER)) {
+        }
+        else if (tier.equalsIgnoreCase(SILVER)) {
             this.newEpoch = oldEpoch - 1000;
-        } else {
+        } 
+        else {
             this.newEpoch = oldEpoch - 0;
         }
-
+        
         newEpochHolder = this.newEpoch;
-
+        
         if (!stack.isEmpty()) {
             if (stack.contains(newEpochHolder)) {
                 newEpochHolder += 1L;
             }
         }
-
         if (stack.size() == 1800) {
             stack.clear();
         }
-
+        
         this.newEpoch = newEpochHolder;
-        this.txnld = txnld;
+        stack.push(this.newEpoch);
+
+        this.oldEpoch = oldEpoch;
+        this.txnId = txnId;
         this.tier = tier;
-
     }
 
-    public Long getOldEpoch() {
-        return oldEpoch;
-    }
+    public Long getOldEpoch() {return oldEpoch;}
+    
+    public Long getNewEpoch() {return newEpoch;}
 
-    public Long getNewEpoch() {
-        return newEpoch;
-    }
+    public String getTxnId() {return txnId;}
 
-    public String getTxnld() {
-        return txnld;
-    }
-
-    public String getTier() {
-        return tier;
-    }
-
+    public String getTier() {return tier;}
+    
+    @Override 
     public int compareTo(Payment otherTransaction) {
         return this.getNewEpoch().compareTo(otherTransaction.newEpoch);
     }
-
+    
+    @Override
     public String toString() {
-        return this.getTxnld();
+        return this.getTxnId();
     }
-
+    
     public static String toStr(PriorityQueue<Payment> q) {
         if (stack.size() == 150) {
 
         }
         String result = "";
         int i = 0;
-        while (!q.isEmpty() && i < 100) {
+        while(!q.isEmpty() && i < 100){
             result += q.poll() + " ";
             i++;
         }
         return result.trim();
     }
-
+    
     public static void main(String[] args) {
+        // variables block
         final String EXIT = "EXIT";
         final String REBOOT = "REBOOT";
-
+        
         Long epoch;
-        String txnld, tier;
-
+        String txnId, tier;
+        
         int diff = 0;
         Long lastAddedEpoch = 0L;
         boolean pass = false;
-
-        Scanner scan = new Scanner(System.in);
-
-        while (scan.hasNextLine()) {
+        // end of variale block
+                
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNextLine()) {
             try {
-                String in = scan.nextLine();
+                String in = sc.nextLine();
                 in = in.trim();
-
+                
                 if (in.equalsIgnoreCase(EXIT)) {
                     break;
                 } else if (in.equalsIgnoreCase(REBOOT)) {
-                    meows.clear();
+                    meowsPQ.clear();
                 } else {
-                    String[] transaction = in.split("\\s+", 3);
+                    String [] transaction = in.split("\\s+", 3);
                     epoch = Long.valueOf(transaction[0]);
-                    txnld = transaction[1];
+                    txnId = transaction[1];
                     tier = transaction[2];
-
-                    if (!meows.isEmpty()) {
+                    
+                    if (!meowsPQ.isEmpty()) {
                         lastAddedEpoch /= 1000;
-
+                        
                         Long epochTemp = epoch;
                         epochTemp /= 1000;
-
+                        
                         diff = epochTemp.intValue() - lastAddedEpoch.intValue();
                         lastAddedEpoch = epoch;
+                        
                     } else {
                         lastAddedEpoch = epoch;
                     }
-
-                    Payment transactionObj = new Payment(epoch, txnld, tier);
-                    meows.add(transactionObj);
-
+                    
+                    Payment transactionObj = new Payment(epoch, txnId, tier);
+                    meowsPQ.add(transactionObj);
+                    
                     Long round = lastAddedEpoch % 1000;
-                    if (diff == 1 || pass) {
+                    if (diff == 1 || pass) { 
                         if (round.intValue() != 0) {
-                            String ans = toStr(meows);
+                            String ans = toStr(meowsPQ);
                             System.out.println(ans);
                             diff = 0;
                             pass = false;
                         } else {
                             pass = true;
                         }
-                    }
-                }
+                    }  
+                } 
+                
             } catch (InputMismatchException e) {
                 return;
             } catch (NumberFormatException e) {
@@ -144,5 +149,6 @@ public class Payment implements Comparable<Payment> {
                 return;
             }
         }
-    }
+        
+    } 
 }
